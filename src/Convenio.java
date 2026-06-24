f590ca5b172edda318cd626cb90c843e6f471859
Giuli0196 <giulieduardox3@gmail.com>
2026-06-24 03:16:07 -0300

    criação da classe convenio, construtor e resumo
    adição de get e setters, exibição de resumo, criação e construtores e métodos

---
+++ src/Convenio.java
@@ -0,0 +1,30 @@
+public class Convenio {
+    private String nome;
+    private int cobertura;
+
+    public Convenio (String nome, int cobertura){
+        this.nome = nome;
+        this.cobertura = cobertura;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setCobertura(int cobertura) {
+        this.cobertura = cobertura;
+    }
+
+    public int getCobertura() {
+        return cobertura;
+    }
+
+    public void exibirResumo(){
+        System.out.println("nome: "+ nome+"\n" +
+                "cobertura: "+cobertura);
+    }
+}
