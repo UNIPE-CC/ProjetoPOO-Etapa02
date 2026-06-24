ff98eb070ff171aca4b97b6a83f14e12aaa553ff
Giuli0196 <giulieduardox3@gmail.com>
2026-06-24 01:08:03 -0300

    criação do construtor e metodos de acesso
    continuando encapsulamento

--- src/Convenio.java
+++ src/Convenio.java
@@ -1,5 +1,25 @@
 public class Convenio {
     private String nome;
     private int cobertura;
-    
+
+    public Convenio (String nome, int cobertura){
+        this.nome = nome;
+        this.cobertura = cobertura;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setCobertura(int cobertura) {
+        this.cobertura = cobertura;
+    }
+
+    public int getCobertura() {
+        return cobertura;
+    }
 }
