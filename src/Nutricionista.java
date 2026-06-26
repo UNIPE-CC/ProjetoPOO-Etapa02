2ff2c1e57c90e0d5f16e93a92b3ebb0e044bbe6d
Jojoanes <joaovfujarra@gmail.com>
2026-06-26 03:26:43 -0300

    Atualização da Classe Nutricionista

--- src/Nutricionista.java
+++ src/Nutricionista.java
@@ -1,3 +1,28 @@
 public class Nutricionista extends Profissionais{
+    private String planoAlimentar;
 
+    public Nutricionista(String nome, String planoAlimentar) {
+        super(nome, "");
+        this.planoAlimentar = planoAlimentar;
+    }
+
+    public String getPlanoAlimentar() {
+        return planoAlimentar;
+    }
+
+    public void setPlanoAlimentar(String planoAlimentar) {
+        this.planoAlimentar = planoAlimentar;
+    }
+
+    @Override
+    public String exibirResumo() {
+        return "Espec: " + especialidade + " | Reg: " + registroProfissional + " | Plano Alimentar: " + planoAlimentar + " | Valor: R$" + valorConsulta;
+    }
+
+    @Override
+    public String registrarEspecifico(Atendimento atendimento) {
+        atendimento.setDetalhesEspecificos("Plano Alimentar: " + this.planoAlimentar);
+        return "Plano alimentar adicionado ao atendimento.";
+    }
 }
+
