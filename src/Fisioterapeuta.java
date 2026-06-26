14aaef6f70f4f135a9077dcb1cea59c50d3f5f03
Jojoanes <joaovfujarra@gmail.com>
2026-06-26 02:49:58 -0300

    Envio da Classe Fisioterapeuta

--- src/Fisioterapeuta.java
+++ src/Fisioterapeuta.java
@@ -1,3 +1,38 @@
 public class Fisioterapeuta extends Profissional{
+    private int totalSessoesPrevistas;
+    private int sessoes;
+
     
+    public Fisioterapeuta(String nome){
+        super(nome, "");
+        this.totalSessoesPrevistas = totalSessoesPrevistas;
+        this.sessoes = sessoes;
+    }
+    
+    public int getTotalSessoesPrevistas(){
+        return totalSessoesPrevistas;
+    }
+    
+    public void setTotalSessoesPrevistas(int totalSessoesPrevistas){
+        this.totalSessoesPrevistas = totalSessoesPrevistas;
+    }
+    
+    public int getSessoes(){
+        return sessoes;
+    }
+    
+    public void setSessoes(int qntSessoes){
+        this.sessoes = qntSessoes;
+    }
+    
+    
+    @Override
+    public String exibirResumo() {
+        return "Espec: " + especialidade + " | Reg: " + registroProfissional + " | Valor: R$" + valorConsulta;
+    }
+    
+    @Override
+    public String registrarEspecifico(Atendimento atendimento){
+        return atendimento;
+    }
 }
