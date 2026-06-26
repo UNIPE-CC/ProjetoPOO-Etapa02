4b591af0b543eaf00895f971cdf7236ecf48464d
Doglaska <doglaska@gmail.com>
2026-06-26 04:46:33 -0300

    fix: ajuste nas especializacoes

--- src/Psicologo.java
+++ src/Psicologo.java
@@ -1,3 +1,26 @@
 public class Psicologo extends Profissional {
-
+    private String abordagem;
+    
+    public psicologo(String nome){
+        super(nome, "");
+        this.abordagem = abordagem;
+    }
+    
+    public String getAbordagem(){
+        return abordagem;
+    }
+    
+    public void setAbordagem(String abordagem){
+        this.abordagem = abordagem;
+    }
+    
+    @Override
+    public String exibirResumo() {
+        return "Espec: " + super(especialidade) + " | Reg: " + super(registroProfissional) + " | Valor: R$" + super(valorConsulta);
+    }
+    
+    @Override
+    public String registrarEspecifico(Atendimento atendimento){
+        return ("Qual o registro: ");
+    }
 }
