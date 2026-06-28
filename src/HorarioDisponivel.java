d9bb03f6c46fb06d95b79e046c26a19d09c1b639
Ryan Lustosa <Ryanbatistalus.csunipe@gmail.com>
2026-06-27 23:43:43 -0300

    feat: criação classe HorarioDisponivel com atributos básicos

--- src/HorarioDisponivel.java
+++ src/HorarioDisponivel.java
@@ -1,12 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author dogla
- */
 public class HorarioDisponivel {
-    
+    private String diaSemana;
+    private String turno;
+
+    public HorarioDisponivel(String diaSemana, String turno) {
+        this.diaSemana = diaSemana;
+        this.turno = turno;
+    }
+
+    public String getDiaSemana() {
+        return diaSemana;
+    }
+
+    public void setDiaSemana(String diaSemana) {
+        this.diaSemana = diaSemana;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+
+    public String exibirResumo() {
+        return diaSemana + " (" + turno + ")";
+    }
 }
