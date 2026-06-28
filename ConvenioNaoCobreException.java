777ef27ec58decaadc86b657a403ddbf657daefe
RyanBL-coder <Ryanbatistalus.csunipe@gmail.com>
2026-06-28 18:44:28 -0300

    Delete ConvenioNaoCobreException.java

--- ConvenioNaoCobreException.java
+++
@@ -1,9 +0,0 @@
-public class ConvenioNaoCobreException extends Exception {
-    public ConvenioNaoCobreException(String mensagem) {
-        super(mensagem);
-    }
-    
-    public ConvenioNaoCobreException(String mensagem, Throwable causa) {
-        super(mensagem, causa);
-    }
-}
